52561d79403dbcc47dee93f4a8af7c9497ab01e2
Vitor Delgado <vitor.ldelgado@gmail.com>
2024-04-15 00:19:54 -0300

    app modules and etc

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,41 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MovieModule } from './movie/movie.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { CacheModule } from '@nestjs/cache-manager';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    envFilePath: ['.env'],
+  }),
+  CacheModule.register(),
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    entities: [`${__dirname}/**/*.entity{.js,.ts}`],
+    migrations: [`${__dirname}/migration/{.ts,*.js}`],
+    migrationsRun: true,
+  }),
+  ClientsModule.register([
+    {
+      name: 'MOVIE_SERVICE',
+      transport: Transport.REDIS,
+      options: {
+        host: 'localhost',
+        port: 6379,
+      }
+    },
+  ]),
+  MovieModule
+],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule,{
+    transport: Transport.REDIS,
+    options: {
+      host: 'localhost',
+      port: 6379,
+    }
+  });
+
+  const api = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('API documentation')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(api, config);
+  SwaggerModule.setup('api', api, document);
+
+  await api.listen(3000);
+  
+  await app.listen();
 }
 bootstrap();
