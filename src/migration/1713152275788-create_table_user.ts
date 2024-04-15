2a6085d4657848206ed78efb37417f3c2e09f1f8
Vitor Delgado <vitor.ldelgado@gmail.com>
2024-04-15 00:52:46 -0300

    migrations

--- src/migration/1713152275788-create_table_user.ts
+++ src/migration/1713152275788-create_table_user.ts
@@ -1,24 +1,23 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class CreateTableMovie1713143831708 implements MigrationInterface {
+export class CreateTableUser1713152275788 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
-            CREATE TABLE movie (
+            CREATE TABLE "user" (
                 id uuid PRIMARY KEY,
-                title varchar(255) NOT NULL,
-                synopsis text NOT NULL,
-                cover varchar(255) NOT NULL,
-                duration int NOT NULL,
-                rating float NOT NULL,
+                name varchar(255) NOT NULL,
+                email varchar(255) NOT NULL,
+                password varchar(255) NOT NULL,
                 createdAt varchar(255) NOT NULL,
                 updatedAt varchar(255) NOT NULL
             );
         `);
+        
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE movie`);
+        await queryRunner.query(`DROP TABLE "user"`);
     }
 
 }
